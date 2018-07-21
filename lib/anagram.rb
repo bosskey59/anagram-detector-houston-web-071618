b35cca0d6fa4004b25a0c078500ff7e0c4cb1745
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-21 17:30:43 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  def initialize (anagram_word)
+    @anagram_word=anagram_word
+  end
+
+  def match (array_of_words)
+    array_of_words.select do |word|
+      # binding.pry
+      word.split("").sort == @anagram_word.split("").sort
+    end
+  end
+end
